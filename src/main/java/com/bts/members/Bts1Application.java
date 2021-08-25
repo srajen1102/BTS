16fde79009594e1d20aad8220f3892309490af8d
srajen1102 <sharayu.rajendran@gmail.com>
2021-08-25 14:05:41 +0530

    first commit

---
+++ src/main/java/com/bts/members/Bts1Application.java
@@ -0,0 +1,13 @@
+package com.bts.members;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bts1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bts1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bts/members/Bts1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bts.members;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bts1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
