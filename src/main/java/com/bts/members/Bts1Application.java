3a65bb6c5ae3409ba5fd306b8562aa7376236b8d
srajen1102 <sharayu.rajendran@gmail.com>
2021-08-25 16:48:00 +0530

    clone

--- src/main/java/com/bts/members/Bts1Application.java
+++ src/main/java/com/bts/members/Bts1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Bts1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Bts1Application.class, args);
+		SpringApplication.run(Bts1Application.class, args);    
 	}
 
 }
